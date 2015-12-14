816e536602914988d50595eb2b65d5c9f46617e5
student1 <student1@jh-mbair-158-06.d214.org>
2015-12-14 09:54:05 -0600

    First commit of three letter word.

--- ThreeLetterWord/ViewController.swift
+++ ThreeLetterWord/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var currentLetterLabel: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func whenTapped(sender: UITapGestureRecognizer) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
